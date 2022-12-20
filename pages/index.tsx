c871bfd8abfa9b1052be4e52592a651f636998ef
Enric Trenric_trillo <enric_trillo@outlook.com>
2022-12-20 11:44:51 +0000

    c: modded index for base

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,13 @@
 import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import { Fragment } from 'react'
 
 const Home: NextPage = () => {
   return (
-    <div className="">
-      Hello reekosplate
-    </div>
+    <Fragment>
+      <div className="flex items-center justify-center h-screen">
+        <span>Reekosplate</span>
+      </div>
+    </Fragment>
   )
 }
 
