4b7e6bec4e81bdd7a2d7e18321c67b738cf07ce0
Enric Trenric_trillo <enric_trillo@outlook.com>
2022-12-20 16:46:47 +0000

    c: added constants

---
+++ constants/index.tsx
@@ -0,0 +1,5 @@
+export const EMAIL_ADDRESS = 'emailaddy@here.com'
+export const TWITTER_FOLLOW_URL = 'https://twitter.com/intent/follow?screen_name=0xreeko'
+export const GITHUB_URL = 'https://github.com/0xreeko'
+
+export const BLACKLISTED_ROUTES = ['']
